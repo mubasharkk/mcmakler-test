1984a47a4267ab64a31bb77a5fa970815e193650
Mubashar Khokhar <m.khokhar@social-gizmo.com>
2016-07-07 22:07:12 +0200

    Task 8A - Repo function added

--- src/InterviewBundle/Controller/DefaultController.php
+++ src/InterviewBundle/Controller/DefaultController.php
@@ -3,21 +3,22 @@
 namespace InterviewBundle\Controller;
 
 use InterviewBundle\Document\Bio;
-
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
+class DefaultController extends Controller {
 
-class DefaultController extends Controller
-{
-    /**
-     * @Route("/bios")
-     */
-    public function indexAction()
-    {
-	  
+  /**
+   * @Route("/bios")
+   */
+  public function indexAction() {
+
 //	  $bio = new Bio();
 //	  $bio->setName(array(
 //		  'first' => 'Mubashar',
@@ -27,14 +28,26 @@ class DefaultController extends Controller
 //	  // iD : 577e9fddae2be4b4608b456a
 //
 //	  $dm = $this->get('doctrine_mongodb')->getManager();
-	  
-	  $my_bio = $this->get('doctrine_mongodb')
-        ->getRepository('InterviewBundle:Bio')
-			  ;
-//        ->find("51df07b094c6acd67e492f41");
-	  
 
-	  return new Response('My Bio: '.$my_bio->findTotalMatches());
+	$bioRepo = $this->get('doctrine_mongodb')
+			->getRepository('InterviewBundle:Bio')
+	;
+
+	$bios = $bioRepo->findByFirstName('John');
+//        ->find("51df07b094c6acd67e492f41");
+
+	$encoders = array(new JsonEncoder());
+
+	$normalizers = array(new ObjectNormalizer());
+
+	$serializer = new Serializer($normalizers, $encoders);
+
+	$json = $serializer->serialize($bios, 'json');
 	
-    }
+	$response = new Response($serializer->serialize($bios, 'json'));
+	$response->headers->set('Content-Type', 'application/json');
+
+	return $response;
+  }
+
 }
--- src/InterviewBundle/Repository/BioRepository.php
+++ src/InterviewBundle/Repository/BioRepository.php
@@ -4,11 +4,38 @@ namespace InterviewBundle\Repository;
 
 use Doctrine\ODM\MongoDB\DocumentRepository;
 
-class BioRepository extends DocumentRepository
-{
-    public function findTotalMatches()
-    {
-        /* your awesome code block */
-        return 34;
-    }
+class BioRepository extends DocumentRepository {
+
+  /**
+   * Search by First Name
+   * 
+   * @param string $firstName
+   */
+  public function findByFirstName($firstName) {
+	
+	return $this->createQueryBuilder()
+			->field('name.first')->equals($firstName)
+            ->sort('name', 'ASC')
+            ->getQuery()
+            ->execute();
+  }
+
+  /**
+   * Search by First Name
+   * 
+   * @param string $contributionName
+   */  
+  public function findByContribution($contributionName) {
+	
+  }
+
+  /**
+   * Search by Dead before
+   * 
+   * @param string $year
+   */  
+  public function findByDeadBefore($year) {
+	
+  }
+
 }
