f5494b8040ac4d785e562623d3de217effaa906f
Mubashar Khokhar <m.khokhar@social-gizmo.com>
2016-07-07 20:50:33 +0200

    Task #6 - Document for BIO created

--- src/InterviewBundle/Controller/DefaultController.php
+++ src/InterviewBundle/Controller/DefaultController.php
@@ -2,18 +2,38 @@
 
 namespace InterviewBundle\Controller;
 
+use InterviewBundle\Document\Bio;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 
-class DefaultController extends Controller {
 
-  /**
-   * @Route("/", name="")
-   */
-  public function indexAction() {
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/bios")
+     */
+    public function indexAction()
+    {
+	  
+//	  $bio = new Bio();
+//	  $bio->setName(array(
+//		  'first' => 'Mubashar',
+//		  'last' => 'Khokhar'
+//	  ));
+//	  
+//	  // iD : 577e9fddae2be4b4608b456a
+//
+//	  $dm = $this->get('doctrine_mongodb')->getManager();
+	  
+	  $my_bio = $this->get('doctrine_mongodb')
+        ->getRepository('InterviewBundle:Bio')
+        ->find("51df07b094c6acd67e492f41");
+	  
 
+	  return new Response('My Bio: '.$my_bio->getFullname());
 	
-  }
-
+    }
 }
---
+++ src/InterviewBundle/Document/Bio.php
@@ -0,0 +1,202 @@
+<?php
+
+namespace InterviewBundle\Document;
+
+use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
+
+
+/**
+ * Description of Bio
+ *
+ * @author mubasharkk
+ * 
+ * @MongoDB\Document
+ */
+class Bio
+{
+    /**
+     * @MongoDB\Id
+     */
+    protected $id;
+
+    /**
+     * @MongoDB\Field(type="hash")
+     */
+    protected $name;
+
+    /**
+     * @MongoDB\Field(type="string")
+     */
+    protected $title;
+
+    /**
+     * @MongoDB\Field(type="date")
+     */
+    protected $birth;
+
+    /**
+     * @MongoDB\Field(type="date")
+     */
+    protected $death;
+
+    /**
+     * @MongoDB\Field(type="collection")
+     */
+    protected $contrib;
+
+    /**
+     * @MongoDB\Field(type="collection")
+     */
+    protected $awards;
+    
+
+    /**
+     * Get id
+     *
+     * @return id $id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param hash $name
+     * @return self
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return hash $name
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return self
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string $title
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set birth
+     *
+     * @param date $birth
+     * @return self
+     */
+    public function setBirth($birth)
+    {
+        $this->birth = $birth;
+        return $this;
+    }
+
+    /**
+     * Get birth
+     *
+     * @return date $birth
+     */
+    public function getBirth()
+    {
+        return $this->birth;
+    }
+
+    /**
+     * Set death
+     *
+     * @param date $death
+     * @return self
+     */
+    public function setDeath($death)
+    {
+        $this->death = $death;
+        return $this;
+    }
+
+    /**
+     * Get death
+     *
+     * @return date $death
+     */
+    public function getDeath()
+    {
+        return $this->death;
+    }
+
+    /**
+     * Set contrib
+     *
+     * @param collection $contrib
+     * @return self
+     */
+    public function setContrib($contrib)
+    {
+        $this->contrib = $contrib;
+        return $this;
+    }
+
+    /**
+     * Get contrib
+     *
+     * @return collection $contrib
+     */
+    public function getContrib()
+    {
+        return $this->contrib;
+    }
+
+    /**
+     * Set awards
+     *
+     * @param collection $awards
+     * @return self
+     */
+    public function setAwards($awards)
+    {
+        $this->awards = $awards;
+        return $this;
+    }
+
+    /**
+     * Get awards
+     *
+     * @return collection $awards
+     */
+    public function getAwards()
+    {
+        return $this->awards;
+    }
+	
+	/**
+	 * Get Fullname
+	 */
+	
+	public function getFullname(){
+	  return implode(' ', $this->name);
+	}
+}
