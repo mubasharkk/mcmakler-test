6f4f95856a208258545498ecf4a6bec2bd14732d
Mubashar Khokhar <m.khokhar@social-gizmo.com>
2016-07-08 02:51:33 +0200

    Task #10 - Controller created

---
+++ src/InterviewBundle/Controller/ContributionsController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace InterviewBundle\Controller;
+
+use InterviewBundle\Document\Bio;
+use InterviewBundle\Services\BioServices;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Description of ContributionsController
+ *
+ * @author mubasharkk
+ */
+class ContributionsController extends Controller {
+  
+  /**
+   * @Route("/contributions")
+   */
+  
+  function indexAction(){
+	
+  }
+}
